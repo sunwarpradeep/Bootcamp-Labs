bdacfb3102e5e0187836cad87dadee3dbfe25245
sunwarpradeep <sunwarpradeep@yahoo.com>
2024-04-29 14:50:28 -0500

    OnlineStore$

---
+++ Workshop/OnlineStore/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        var screens = new Screens();
+        screens.displayHomeInterface();
+    }
+}
